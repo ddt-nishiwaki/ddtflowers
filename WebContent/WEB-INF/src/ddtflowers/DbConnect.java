b0a4094ee7cfbb468f3958d1b345a52e3e149d00
SeijiNishiwaki <nishiwakiat.wjnisi@gmail.com>
2017-12-15 21:05:35 +0900

    DbConnectクラス実装 修正:formatterによる差分 100%
    formatterの設定が変わってしまったので多数の変更が出ています。
    変更が見づらいので切り分けてコミットいたします。
    問題ありましたら連絡いただけると幸いです。
    よろしくお願いいたします。

--- WebContent/WEB-INF/src/ddtflowers/DbConnect.java
+++ WebContent/WEB-INF/src/ddtflowers/DbConnect.java
@@ -1,8 +1,10 @@
 package ddtflowers;
+
 import java.sql.Connection;
-import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * クラス名:dbConnect
  * 概要  :DBに接続する関数を持つクラス。
@@ -15,26 +17,26 @@ public class DbConnect {
     // constants
     //////////////////////////////////////
     // データベースのホスト設定です。
-    private static final String DB_HOST = "localhost";
+    private static final String DB_HOST     = "localhost";
     // 使用するデータベースの設定です。
-    private static final String DB_NAME = "ddthink-com00006";
+    private static final String DB_NAME     = "ddthink-com00006";
     // データベースのユーザー設定です。
-    private static final String DB_USER = "root";
+    private static final String DB_USER     = "root";
     // データベースのパスワード設定です。
     private static final String DB_PASSWORD = "";
     // データベースの文字コード設定です。
-    private static final String DB_CHARSET = "UTF-8";
+    private static final String DB_CHARSET  = "UTF-8";
     // 使用するデータベースの種類です。
-    private static final String DB_TYPE = "mysql";
+    private static final String DB_TYPE     = "mysql";
     // データベース接続に使用するドライバー名です。
-    private static final String DB_DRIVER = "jdbc";
+    private static final String DB_DRIVER   = "jdbc";
     // JDBCドライバーで接続するためのURL設定です。
-    private static final String DB_URL = DB_DRIVER + ":" + DB_TYPE + "://" + DB_HOST + "/" + DB_NAME;
+    private static final String DB_URL      = DB_DRIVER + ":" + DB_TYPE + "://" + DB_HOST + "/" + DB_NAME;
     //////////////////////////////////////
     // member
     //////////////////////////////////////
     // コネクションを保持するためのメンバーです。
-    private Connection mDbConnect = null;
+    private Connection          mDbConnect  = null;
     //////////////////////////////////////
     // class variable
     //////////////////////////////////////
@@ -51,9 +53,10 @@ public class DbConnect {
      * 作成日:20171205
      * 戻り値:Connection
      */
-    public Connection getConnect () {
+    public Connection getConnect() {
         return mDbConnect;
     }
+
     //////////////////////////////////////
     // method
     //////////////////////////////////////
@@ -67,16 +70,17 @@ public class DbConnect {
      * 作成日:20171205
      * 戻り値:void
      */
-    public void connect() throws Exception{
+    public void connect() throws Exception {
         // JDBCドライバで扱うDBを設定する(今回はmysql)
         Class.forName("com.mysql.jdbc.Driver");
         // DBとの接続を取得する
-        mDbConnect =  DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        mDbConnect = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
         // 文字コード設定を行うクエリ発行のため、ステートメントを取得する
         sDbh = mDbConnect.createStatement();
         // クエリの文字コード設定をUTF8に設定する
         sDbh.executeQuery("SET NAMES utf8");
     }
+
     /**
      * DB接続を切断する
      * 関数名:disconnect
